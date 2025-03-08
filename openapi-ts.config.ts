b04a053c7ce66a642c0d0f3a397cdede4c8a82f2
Jack Sanders <jacklewissanders@gmail.com>
2025-03-08 17:15:35 -0600

    hey-api

---
+++ openapi-ts.config.ts
@@ -0,0 +1,15 @@
+import { defaultPlugins } from '@hey-api/openapi-ts';
+
+export default {
+  input: 'http://127.0.0.1:8000/api-docs/openapi.json',
+  output: {
+    format: 'prettier',
+    lint: 'eslint',
+    path: './src/client',
+  },
+  plugins: [
+    ...defaultPlugins,
+    '@hey-api/client-fetch',
+    '@tanstack/svelte-query', 
+  ],
+};
