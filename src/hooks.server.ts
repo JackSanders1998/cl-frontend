0f4ae0305f60c237d100b91af10383dbc9dbe411
Jack Sanders <jacklewissanders@gmail.com>
2025-03-23 18:28:52 -0500

    troubleshoot

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,13 +3,12 @@ import { sequence } from '@sveltejs/kit/hooks';
 import { Security } from './utils/security';
 import { withClerkHandler } from 'svelte-clerk/server';
 import { client } from './client/client.gen';
-
-const BASE_URL = 'http://127.0.0.1:8000';
+import { CL_BACKEND_URL as baseUrl } from '$env/static/private';
 
 export const handle: Handle = sequence(withClerkHandler(), async ({ event, resolve }) => {
 	client.setConfig({
 		auth: async () => `Bearer ${await event.locals.auth.getToken()}`,
-		baseUrl: BASE_URL
+		baseUrl
 	});
 
 	event.locals.security = new Security(event);
